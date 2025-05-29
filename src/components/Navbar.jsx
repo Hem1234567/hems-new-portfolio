857127aa3964624052a359232e73bb0fbd8e7cea
HEMKUMAR <68186793+Hem1234567@users.noreply.github.com>
2025-05-29 11:15:52 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center items-center py-4 relative">
           {/* Desktop Navigation */}
-          <div className="hidden md:flex space-x-18 font-bold">
+          <div className="hidden md:flex space-x-16 font-bold">
             {navItems.map((item, index) => (
               <a
                 key={item.href}
